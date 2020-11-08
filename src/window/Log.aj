4b035e6967351feff89f0f3d89e4f7bffaaca506
maryta22 <maria.mariaog.rivera@gmail.com>
2020-11-07 20:21:05 -0500

    Cambios realizados

--- src/window/Log.aj
+++ src/window/Log.aj
@@ -1,10 +1,45 @@
 package window;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.aspectj.lang.reflect.Pointcut;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
 public aspect Log {
-		
+		File file = new File("log.txt");
+
 		pointcut callChange(): call(* change*(..) ); 
 	    after() : callChange() {
 			System.out.println("New background color: "+ Window.currentColor);
 	    } 
+	    
+	    pointcut callChange2(Window w): call(* change*(..) )&& this(w); 
+	    after(Window w) : callChange2(w) {
+	    	if(!Window.currentColor.equals("Palegreen")) {
+	    		w.getCuadro().getChildren().add(new Label("Cambio realizado"));
+	    	}			
+	    }
+	    
+	    pointcut callChange3(Window w): call(* change*(..) ) && this(w); 
+	    after(Window w) : callChange3(w) {
+	    	if(!Window.currentColor.equals("Skyblue")) {
+	    		w.getCuadro().getChildren().add(new Label("Cambio Guardado"));   		
+	    			try {
+						BufferedWriter bw = new BufferedWriter(new FileWriter(file,true));
+						bw.write(w.currentColor + "\n");
+			    		bw.close();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}	
+	    	}
+	    }
 	    
 }
--- src/window/Window.java
+++ src/window/Window.java
@@ -10,55 +10,66 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 
-public class Window{
-	
-	private Scene scene;
-	private VBox root;
-	final String[] COLORS = {"Skyblue","Yellow","Palegreen"};
-	static String currentColor;
-	
-	public Window() {
-		root = createRoot();
-		scene = createScene();
-	}
-	
-	private VBox createRoot() {
-		Label title = new Label("Buttons");
-		title.setFont(Font.font("Californian FB", FontPosture.ITALIC, 25));
-		title.setTextFill(Color.PURPLE);
-		HBox buttons = createButtons();
-		VBox vb = new VBox(title,buttons);
-		vb.setSpacing(30);
-		vb.setAlignment(Pos.CENTER);
-		return vb;
-	}
-	
-	private HBox createButtons() {
-		HBox hb = new HBox();
-		for(int i = 0; i < COLORS.length; i++) {
-			Button b = new Button(COLORS[i]);
-			hb.getChildren().addAll(b);
-			b.setOnAction(e -> {
-				currentColor = b.getText();
-				changeColor();
-			});
-		}
-		hb.setSpacing(10);
-		hb.setAlignment(Pos.CENTER);
-		return hb;
-	}
-	
-	private void changeColor() {
-		root.setStyle("-fx-background-color: "+currentColor+";");
-	}
-	
-	private Scene createScene() {
-		Scene sc = new Scene(root,300,200);
-		return sc;
-	}
-	
-	public Scene getScene() {
-		return this.scene;
-	}
-	
+public class Window {
+
+    private Scene scene;
+    private VBox root;
+    private VBox cuadro;
+    final String[] COLORS = {"Skyblue", "Yellow", "Palegreen"};
+    static String currentColor;
+
+    public Window() {
+        root = createRoot();
+        scene = createScene();
+    }
+
+    private VBox createRoot() {
+        Label title = new Label("Buttons");
+        title.setFont(Font.font("Californian FB", FontPosture.ITALIC, 25));
+        title.setTextFill(Color.PURPLE);
+        HBox buttons = createButtons();
+        cuadro = new VBox();
+        cuadro.setAlignment(Pos.CENTER);
+        VBox vb = new VBox(title, buttons, cuadro);
+        vb.setSpacing(30);
+        vb.setAlignment(Pos.CENTER);
+        return vb;
+    }
+
+    private HBox createButtons() {
+        HBox hb = new HBox();
+        for (int i = 0; i < COLORS.length; i++) {
+            Button b = new Button(COLORS[i]);
+            hb.getChildren().addAll(b);
+            b.setOnAction(e -> {
+                currentColor = b.getText();
+                changeColor();
+            });
+        }
+        hb.setSpacing(10);
+        hb.setAlignment(Pos.CENTER);
+        return hb;
+    }
+
+    private void changeColor() {
+        root.setStyle("-fx-background-color: " + currentColor + ";");
+    }
+
+    private Scene createScene() {
+        Scene sc = new Scene(root, 300, 200);
+        return sc;
+    }
+
+    public Scene getScene() {
+        return this.scene;
+    }
+
+    public VBox getRoot() {
+        return root;
+    }
+
+    public VBox getCuadro() {
+        return cuadro;
+    }
+
 }
